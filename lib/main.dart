7f640803a5883989b9cee29311b2d6784d3e93cf
Edwin Molina <edwinmg27@gmail.com>
2024-02-04 18:45:21 -0400

    add widget counter screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/widgets/counter_screen.dart';
 
 // Toda aplicacion de flutter tiene en su main una ejecucion de un widget inicial, entiendase de widget inicial como nuestra aplicacion como tal. eso se ejecuta con una funcion que se llama runApp(); la funcion  importara el package material.dart
 void main() {
-  runApp(MyApp()); // runApp pedira un widget, cuando miramos que algo recibe un widget literalmente puede ser cualquier widget
+  runApp(
+      const MyApp()); // runApp pedira un widget, cuando miramos que algo recibe un widget literalmente puede ser cualquier widget
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(child: Text('Hola mundo')), // center widget se encarga de centrar su hijo en las dimensiones disponibles del padre.
+      home: CounterScreen(),
     );
   }
-
 }
