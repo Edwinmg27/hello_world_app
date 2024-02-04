913a2e58c121dd700797020b8bb8f14fa1e1f5b4
Edwin Molina <edwinmg27@gmail.com>
2024-02-04 14:19:09 -0400

    Add stateleswidget to hola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+// Toda aplicacion de flutter tiene en su main una ejecucion de un widget inicial, entiendase de widget inicial como nuestra aplicacion como tal. eso se ejecuta con una funcion que se llama runApp(); la funcion  importara el package material.dart
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // runApp pedira un widget, cuando miramos que algo recibe un widget literalmente puede ser cualquier widget
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(child: Text('Hola mundo')), // center widget se encarga de centrar su hijo en las dimensiones disponibles del padre.
+    );
   }
+
 }
